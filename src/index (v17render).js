997a86d76736c83e5a25f6ecdda1554dad796519
nxnikolaos <nxnatlas@gmail.com>
2023-01-12 17:52:48 +0200

    First commit

--- src/index (v17render).js
+++ src/index (v17render).js
@@ -1,5 +1,5 @@
 import React from "react";
-import ReactDOM from "react-dom/client";
+import ReactDom from "react-dom";
 
 //CSS
 import "./index.css";
@@ -34,21 +34,15 @@ function BookList() {
   );
 }
 
-const Book = ({ img, title, author }) => {
-  //const { img, title, author } = props; destructuring of objects
+const Book = (props) => {
+  console.log(props);
   return (
     <article className="book">
-      <img src={img} alt="book-cover"></img>
-      <h1>{title}</h1>
-      <h4>{author}</h4>
+      <img src={props.img} alt="book-cover"></img>
+      <h1>{props.title}</h1>
+      <h4>{props.author}</h4>
     </article>
   );
 };
 
-//React v17 render
-//ReactDΟΜ.render(<BookList />, document.getElementById("root"));
-
-//React v18 render
-const container = document.getElementById("root");
-const rootContainer = ReactDOM.createRoot(container);
-rootContainer.render(<BookList />);
+ReactDom.render(<BookList />, document.getElementById("root"));
