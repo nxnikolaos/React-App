73ff0ff2571987311af4c9c41002318abf1005d0
nxnikolaos <nxnatlas@gmail.com>
2023-01-13 15:34:21 +0200

    Key Prop and Spread Operator.

--- src/index.js
+++ src/index.js
@@ -7,16 +7,19 @@ import "./index.css";
 //setup vars
 const books = [
   {
+    id: 1,
     img: "https://images-na.ssl-images-amazon.com/images/I/81O1oy0y9eL._AC_UL210_SR195,210_.jpg",
     title: "Where the Crawdads SIng",
     author: "Delia Owens",
   },
   {
+    id: 2,
     img: "https://images-na.ssl-images-amazon.com/images/I/41ZLnc34EiL._AC_UL300_SR300,200_.jpg",
     title: "Verity",
     author: "Colleen Hoover",
   },
   {
+    id: 3,
     img: "https://images-na.ssl-images-amazon.com/images/I/41ZLnc34EiL._AC_UL300_SR300,200_.jpg",
     title: "Verity",
     author: "Colleen Hoover",
@@ -27,15 +30,14 @@ function BookList() {
   return (
     <section className="booklist">
       {books.map((book) => {
-        return <Book book={book}></Book>;
+        return <Book key={book.id} {...book}></Book>;
       })}
     </section>
   );
 }
 
-const Book = (props) => {
-  const { img, title, author } = props.book; //destructuring of objects
-  console.log(props);
+const Book = ({ img, title, author }) => {
+  // const { img, title, author } = props;
   return (
     <article className="book">
       <img src={img} alt="book-cover"></img>
